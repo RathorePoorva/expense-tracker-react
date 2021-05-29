0308228913aaec16b30abe0a48bf82afb1ebe6a5
Poorva Rathore <poorva.rathore1@gmail.com>
2021-05-29 17:14:48 +0530

    basic

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 
 const App = () => {
+  const expense = [
+    {
+      id: "e1",
+      title: "Electricity",
+      date: new Date(2021, 4, 21),
+      amount: "2393",
+    },
+    {
+      id: "e2",
+      title: "Grocery",
+      date: new Date(2021, 4, 20),
+      amount: "10000",
+    },
+    { id: "e3", title: "Rent", date: new Date(2021, 4, 1), amount: "25000" },
+    { id: "e4", title: "Gifts", date: new Date(2021, 4, 29), amount: "1500" },
+  ];
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseList expense={expense} />
     </div>
   );
 };
---
+++ src/component/ExpenseList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./css/Expense.css";
+import ExpenseItem from "./ExpenseItem";
+const ExpenseList = (props) => {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.expense[0].title}
+        amount={props.expense[0].amount}
+        date={props.expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[1].title}
+        amount={props.expense[1].amount}
+        date={props.expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[2].title}
+        amount={props.expense[2].amount}
+        date={props.expense[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[3].title}
+        amount={props.expense[3].amount}
+        date={props.expense[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+};
+
+export default ExpenseList;
